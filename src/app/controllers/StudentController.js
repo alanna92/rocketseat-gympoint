fdf7634f41a89498f571a1c3ee529bc762fb1ea7
Alanna Vanzella <alanna92@gmail.com>
2019-10-24 22:53:19 -0300

    First commit

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,85 @@
+import * as Yup from 'yup';
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      age: Yup.number().required(),
+      weight: Yup.number().required(),
+      height: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists' });
+    }
+
+    const { id, name, email, weight, height, age } = await Student.create(
+      req.body
+    );
+
+    return res.json({
+      id,
+      name,
+      email,
+      weight,
+      height,
+      age,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      age: Yup.number(),
+      weight: Yup.number(),
+      height: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const { email } = req.body;
+    const { id } = req.params;
+
+    const student = await Student.findByPk(id);
+
+    if (!student) {
+      return res.status(400).json({ error: 'Student not found' });
+    }
+
+    if (email && email !== student.email) {
+      const studentExists = await Student.findOne({ where: { email } });
+
+      if (studentExists) {
+        return res.status(400).json({ error: 'Student already exists' });
+      }
+    }
+
+    const { name, weight, height, age } = await student.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      weight,
+      height,
+      age,
+    });
+  }
+}
+
+export default new StudentController();
