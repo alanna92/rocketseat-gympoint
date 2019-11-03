c84cea8c5febecb571851210adaeee3f7b762397
Alanna Vanzella <alanna92@gmail.com>
2019-11-03 17:43:58 -0200

    Add plan

---
+++ src/app/controllers/PlanController.js
@@ -0,0 +1,93 @@
+import * as Yup from 'yup';
+import Plan from '../models/Plan';
+
+class PlanController {
+  async index(req, res) {
+    const plans = await Plan.findAll({
+      attributes: ['id', 'title', 'duration', 'price'],
+    });
+    return res.json(plans);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      duration: Yup.number().required(),
+      price: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const planExists = await Plan.findOne({
+      where: { title: req.body.title },
+    });
+
+    if (planExists) {
+      return res.status(400).json({ error: 'Plan already exists' });
+    }
+
+    const { id, title, duration, price } = await Plan.create(req.body);
+
+    return res.json({
+      id,
+      title,
+      duration,
+      price,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      duration: Yup.number(),
+      price: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const plan = await Plan.findByPk(req.params.id);
+
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan not found' });
+    }
+
+    const { title } = req.body;
+
+    if (title && title !== plan.title) {
+      const planExists = await Plan.findOne({
+        where: { title: req.body.title },
+      });
+
+      if (planExists) {
+        return res.status(400).json({ error: 'Plan already exists' });
+      }
+    }
+
+    const { id, duration, price } = await plan.save(req.body);
+
+    return res.json({
+      id,
+      title,
+      duration,
+      price,
+    });
+  }
+
+  async delete(req, res) {
+    const plan = await Plan.findByPk(req.params.id);
+
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan not found' });
+    }
+
+    await plan.destroy();
+
+    return res.json({ message: 'Plan successfully removed' });
+  }
+}
+
+export default new PlanController();
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 import StudentController from './app/controllers/StudentController';
+import PlanController from './app/controllers/PlanController';
 
 const routes = new Router();
 
@@ -16,5 +17,10 @@ routes.put('/users', UserController.update);
 
 routes.post('/students', StudentController.store);
 routes.put('/students/:id', StudentController.update);
+
+routes.get('/plans', PlanController.index);
+routes.post('/plans', PlanController.store);
+routes.put('/plans/:id', PlanController.update);
+routes.delete('/plans/:id', PlanController.delete);
 
 export default routes;
